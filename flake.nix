9e075ff4460bd284b9bf374c7592600431612d16
Simon Mertins <simon@mertins.net>
2026-04-06 00:05:15 +0200

    tree

--- flake.nix
+++ flake.nix
@@ -8,6 +8,20 @@
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+  outputs = inputs: let
+    inherit (inputs.nixpkgs) lib;
+    inherit (lib.fileset) toList fileFilter;
+
+    isNixModule = file:
+      file.hasExt "nix"
+      && file.name != "flake.nix"
+      && !lib.hasPrefix "_" file.name;
+
+    importTree = path:
+      toList (fileFilter isNixModule path);
+
+    mkFlake = inputs.flake-parts.lib.mkFlake {inherit inputs;};
+  in
+    mkFlake {imports = importTree ./.;};
 }
 
--- modules/features/neovim.nix
+++ modules/features/neovim.nix
@@ -3,6 +3,7 @@
   flake.nixosModules.neovim = { pkgs, lib, ...}: {
 
     environment.systemPackages = with pkgs;[
+      vimPlugins.nvim-treesitter
       neovim
       tree-sitter
       lua
