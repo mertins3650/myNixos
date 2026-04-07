586778433f0866b93ecba27375787ffec3ff3f56
Simon Mertins <simon@mertins.net>
2026-04-07 16:05:17 +0200

    push

--- modules/homemanager/hyprland.nix
+++ modules/homemanager/hyprland.nix
@@ -189,7 +189,7 @@
                     gaps_in = 5;
                     gaps_out = 10;
                     border_size = 2;
-                    "col.active_border" = "rgb(ebbcba) rgb(31748f) rgb(eb6f92s) rgb(c4a7e7) 90deg";
+                    "col.active_border" = "rgb(ebbcba) rgb(31748f) rgb(eb6f92) rgb(c4a7e7) 90deg";
                     "col.inactive_border" = "rgba(595959aa)";
                     resize_on_border = false;
                     allow_tearing = false;
