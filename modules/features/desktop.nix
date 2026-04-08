34d713d350aeda90ae61a97980549c188a00eb4a
Simon Mertins <simon@mertins.net>
2026-04-08 21:19:15 +0200

    chr

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -1,42 +1,41 @@
-{self, input, ... }: {
-  flake.nixosModules.desktopenv = { pkgs, lib, ...}: {
+{ self, input, ... }: {
+    flake.nixosModules.desktopenv = { pkgs, lib, ... }: {
+        programs.hyprland = {
+            enable = true;
+            withUWSM = true;
+            xwayland.enable = true;
+        };
 
-    programs.hyprland = {
-      enable = true;
-      withUWSM = true;
-      xwayland.enable = true;
+        programs.chromium = {
+            enable = true;
+            extensions = [
+                "dbepggeogbaibhgnhhndojpepiihcmeb;https://clients2.google.com/service/update2/crx" # vimium
+                "aeblfdkhhhdcdjpifhhbdiojplfjncoa;https://clients2.google.com/service/update2/crx" # 1password
+                "eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # dark reader
+            ];
+        };
+
+        environment.systemPackages = with pkgs; [
+            chromium
+            nautilus
+            fontconfig
+            swayosd
+            mako
+            wl-clipboard
+            xdg-desktop-portal-gtk
+            xdg-desktop-portal-hyprland
+            swaybg
+            hypridle
+            hyprlock
+            fcitx5
+            fcitx5-gtk
+            libsForQt5.fcitx5-qt
+            yaru-theme
+        ];
+
+        fonts.packages = with pkgs; [
+            nerd-fonts.caskaydia-mono
+            font-awesome
+        ];
     };
-
-  programs.chromium = {
-    
-enable = true;
-extensions = [
-"dbepggeogbaibhgnhhndojpepiihcmeb;https://clients2.google.com/service/update2/crx" # vimium 
-"aeblfdkhhhdcdjpifhhbdiojplfjncoa;https://clients2.google.com/service/update2/crx" # 1password 
-"eimadpbcbfnmbkopoojfekhnkhdbieeh;https://clients2.google.com/service/update2/crx" # dark reader
-      
-    
-];
-};
-    environment.systemPackages = with pkgs;[
-      nautilus
-      fontconfig
-      swayosd
-      mako
-      wl-clipboard
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-hyprland
-      swaybg
-      hypridle
-      hyprlock
-      fcitx5
-      fcitx5-gtk
-      libsForQt5.fcitx5-qt
-      yaru-theme
-    ];
-    fonts.packages = with pkgs; [
-      nerd-fonts.caskaydia-mono
-      font-awesome
-    ];
-  };
 }
