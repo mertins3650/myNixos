7791203cce0d0843c0b13f03527edc515eb21ab7
Simon Mertins <simon@mertins.net>
2026-04-08 21:32:00 +0200

    key

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -83,15 +83,14 @@
             };
         };
 
-    systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
+        systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
 
-    security.pam.services.sddm.enableGnomeKeyring = true;
-    security.pam.services.sddm-autologin.enableGnomeKeyring = true;
+        security.pam.services.sddm.enableGnomeKeyring = true;
+        security.pam.services.sddm-autologin.enableGnomeKeyring = true;
 
-    security.pam.services.sddm-autologin.text = pkgs.lib.mkBefore ''
-        auth optional ${pkgs.systemd}/lib/security/pam_systemd_loadkey.so
-        auth include sddm
-    '';
-};
+        security.pam.services.sddm-autologin.text = pkgs.lib.mkBefore ''
+            auth optional ${pkgs.systemd}/lib/security/pam_systemd_loadkey.so
+            auth include sddm
+        '';
     };
 }
