7b7f75d920621bef4bf8e41eb4a16b576c1e9577
Simon Mertins <simon@mertins.net>
2026-04-08 19:50:41 +0200

    ply

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -1,52 +1,88 @@
-{self, input, ... }: {
-  flake.nixosModules.base = { pkgs, lib, ...}: {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-    boot = {
-        kernelParams = [ "quiet" "splash" ];
+{ self, input, ... }: {
+    flake.nixosModules.base = { pkgs, lib, ... }:
+    let
+        archPlymouthTheme = pkgs.stdenvNoCC.mkDerivation {
+            pname = "arch-plymouth-theme";
+            version = "1.0";
 
-        loader = {
-            timeout = 3;
+            src = ../../defaults/plymouth;
 
-            efi.canTouchEfiVariables = true;
+            installPhase = ''
+                mkdir -p $out/share/plymouth/themes/arch
+                cp -r ./* $out/share/plymouth/themes/arch/
+            '';
+        };
+    in {
+        boot.kernelPackages = pkgs.linuxPackages_latest;
 
-            limine = {
+        boot = {
+            kernelParams = [
+                "quiet"
+                "splash"
+                "udev.log_level=3"
+                "systemd.show_status=auto"
+            ];
+
+            consoleLogLevel = 3;
+
+            initrd = {
+                verbose = false;
+                systemd.enable = true;
+            };
+
+            plymouth = {
                 enable = true;
-                maxGenerations = 5;
-                panicOnChecksumMismatch = false;
+                theme = "arch";
+                themePackages = [ archPlymouthTheme ];
+            };
 
-                extraConfig = ''
-                    interface_branding: NixOS
-                    interface_branding_color: 2
+            loader = {
+                timeout = 3;
 
-                    term_background: 1a1b26
-                    backdrop: 1a1b26
+                efi.canTouchEfiVariables = true;
 
-                    term_palette: 15161e;f7768e;9ece6a;e0af68;7aa2f7;bb9af7;7dcfff;a9b1d6
-                    term_palette_bright: 414868;f7768e;9ece6a;e0af68;7aa2f7;bb9af7;7dcfff;c0caf5
+                limine = {
+                    enable = true;
+                    maxGenerations = 5;
+                    panicOnChecksumMismatch = false;
 
-                    term_foreground: c0caf5
-                    term_foreground_bright: c0caf5
-                    term_background_bright: 24283b
-                '';
+                    extraConfig = ''
+                        interface_branding: NixOS
+                        interface_branding_color: 2
+
+                        term_background: 1a1b26
+                        backdrop: 1a1b26
+
+                        term_palette: 15161e;f7768e;9ece6a;e0af68;7aa2f7;bb9af7;7dcfff;a9b1d6
+                        term_palette_bright: 414868;f7768e;9ece6a;e0af68;7aa2f7;bb9af7;7dcfff;c0caf5
+
+                        term_foreground: c0caf5
+                        term_foreground_bright: c0caf5
+                        term_background_bright: 24283b
+                    '';
+                };
             };
         };
+
+        services.displayManager = {
+            defaultSession = "hyprland-uwsm";
+
+            sddm = {
+                enable = true;
+                wayland.enable = true;
+            };
+
+            autoLogin = {
+                enable = true;
+                user = "simonm";
+            };
+        };
+
+        systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
+
+        security.pam.services.sddm-autologin.text = pkgs.lib.mkBefore ''
+            auth optional ${pkgs.systemd}/lib/security/pam_systemd_loadkey.so
+            auth include sddm
+        '';
     };
-services.displayManager = {
-  defaultSession = "hyprland-uwsm";
-  sddm = {
-    enable = true;
-    wayland.enable = true;
-  };
-  autoLogin = {
-    enable = true;    
-    user = "simonm"; # Replace with the desired user
-  };
-};
-  boot.initrd.systemd.enable = true;
-  systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
-  security.pam.services.sddm-autologin.text = pkgs.lib.mkBefore ''
-    auth optional ${pkgs.systemd}/lib/security/pam_systemd_loadkey.so
-    auth include sddm
-  '';
-};
 }
