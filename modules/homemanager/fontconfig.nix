07d128c3d27131c61bd6b4dc7cf24a3b26c5b0fb
Simon Mertins <simon@mertins.net>
2026-04-09 19:53:03 +0200

    up

---
+++ modules/homemanager/fontconfig.nix
@@ -0,0 +1,68 @@
+{ lib, pkgs, ... }:
+{
+    flake.homeModules.fontconfig =
+        { config, lib, ... }:
+        let
+            cfg = config.myModules.fontconfig;
+        in
+        {
+            options.myModules.fontconfig = {
+                enable = lib.mkEnableOption "fontconfig";
+            };
+
+            config = lib.mkIf cfg.enable {
+                fonts.fontconfig = {
+                    enable = true;
+
+                    defaultFonts = {
+                        sansSerif = [ "Liberation Sans" ];
+                        serif = [ "Liberation Serif" ];
+                        monospace = [ "CaskaydiaMono Nerd Font" ];
+                    };
+
+                    configFile."90-custom-aliases" = {
+                        enable = true;
+                        text = ''
+                            <?xml version="1.0"?>
+                            <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+                            <fontconfig>
+                              <alias>
+                                <family>system-ui</family>
+                                <prefer>
+                                  <family>Liberation Sans</family>
+                                </prefer>
+                              </alias>
+
+                              <alias>
+                                <family>ui-monospace</family>
+                                <default>
+                                  <family>monospace</family>
+                                </default>
+                              </alias>
+
+                              <alias>
+                                <family>-apple-system</family>
+                                <prefer>
+                                  <family>Liberation Sans</family>
+                                </prefer>
+                              </alias>
+
+                              <alias>
+                                <family>BlinkMacSystemFont</family>
+                                <prefer>
+                                  <family>Liberation Sans</family>
+                                </prefer>
+                              </alias>
+                            </fontconfig>
+                        '';
+                        priority = 90;
+                    };
+                };
+
+                home.packages = with pkgs; [
+                    liberation_ttf
+                    nerd-fonts.caskaydia-mono
+                ];
+            };
+        };
+}
