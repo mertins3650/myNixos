4fe5cd8dea287fee2f2999c9431b41c4cf831709
Simon Mertins <simon@mertins.net>
2026-04-11 13:30:10 +0200

    test

---
+++ modules/scripts/tmux-sessionizer.nix
@@ -0,0 +1,27 @@
+{ ... }:
+{
+    flake.homeModules.scripts =
+        { config, pkgs, ... }:
+        {
+            home.file.".tmux-sessionizer".source =
+                ./tmux-sessionizer/.tmux-sessionizer;
+
+            xdg.configFile."tmux-sessionizer/tmux-sessionizer.conf".source =
+                ./tmux-sessionizer/tmux-sessionizer.conf;
+
+            home.file.".local/bin/tmux-sessionizer" = {
+                source = ./tmux-sessionizer/tmux-sessionizer;
+                executable = true;
+            };
+
+            home.packages = with pkgs; [
+                tmux
+                fzf
+                findutils
+                gnugrep
+                coreutils
+                procps
+                neovim
+            ];
+        };
+}
