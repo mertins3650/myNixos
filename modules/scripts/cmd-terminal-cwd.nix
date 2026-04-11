503047a4f1080122fd780ba02b983c5b9bc65b56
Simon Mertins <simon@mertins.net>
2026-04-11 12:40:17 +0200

    test

--- modules/scripts/cmd-terminal-cwd.nix
+++ modules/scripts/cmd-terminal-cwd.nix
@@ -1,33 +1,37 @@
 { ... }:
 {
     flake.homeModules.scripts =
-        { config, pkgs, ... }: {
-(pkgs.writeShellApplication {
-    name = "cmd-terminal-cwd";
-    runtimeInputs = with pkgs; [
-        coreutils
-        gawk
-        gnugrep
-        hyprland
-        procps
-    ];
+        { config, pkgs, ... }:
+        {
+            home.packages = [
+                (pkgs.writeShellApplication {
+                    name = "cmd-terminal-cwd";
+                    runtimeInputs = with pkgs; [
+                        coreutils
+                        gawk
+                        gnugrep
+                        hyprland
+                        procps
+                    ];
 
-    text = ''
-        terminal_pid=$(hyprctl activewindow | awk '/pid:/ {print $2}')
-        shell_pid=$(pgrep -P "$terminal_pid" | tail -n1)
+                    text = ''
+                        terminal_pid=$(hyprctl activewindow | awk '/pid:/ {print $2}')
+                        shell_pid=$(pgrep -P "$terminal_pid" | tail -n1)
 
-        if [[ -n "$shell_pid" ]]; then
-            cwd=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
-            shell=$(readlink -f "/proc/$shell_pid/exe" 2>/dev/null)
+                        if [[ -n "$shell_pid" ]]; then
+                            cwd=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
+                            shell=$(readlink -f "/proc/$shell_pid/exe" 2>/dev/null)
 
-            if grep -qs "$shell" /etc/shells && [[ -d "$cwd" ]]; then
-                echo "$cwd"
-            else
-                echo "$HOME"
-            fi
-        else
-            echo "$HOME"
-        fi
-    '';
-})
+                            if grep -qs "$shell" /etc/shells && [[ -d "$cwd" ]]; then
+                                echo "$cwd"
+                            else
+                                echo "$HOME"
+                            fi
+                        else
+                            echo "$HOME"
+                        fi
+                    '';
+                })
+            ];
+        };
 }
--- modules/scripts/floating-terminal.nix
+++ modules/scripts/floating-terminal.nix
@@ -1,19 +1,52 @@
 { ... }:
 {
     flake.homeModules.scripts =
-        { config, pkgs, ... }: {
-(pkgs.writeShellApplication {
-    name = "floating-terminal";
-    runtimeInputs = with pkgs; [
-        bash
-        util-linux
-        uwsm
-        xdg-terminal-exec
-    ];
+        { config, pkgs, ... }:
+        {
+            home.packages = [
+                (pkgs.writeShellApplication {
+                    name = "floating-terminal";
+                    runtimeInputs = with pkgs; [
+                        bash
+                        util-linux
+                        uwsm
+                        xdg-terminal-exec
+                    ];
 
-    text = ''
-        cmd="$*"
-        exec setsid uwsm app -- xdg-terminal-exec --app-id=org.nixy.terminal --title=nixy -e bash -c "$cmd"
-    '';
-})
+                    text = ''
+                        cmd="$*"
+                        exec setsid uwsm app -- xdg-terminal-exec --app-id=org.nixy.terminal --title=nixy -e bash -c "$cmd"
+                    '';
+                })
+
+                (pkgs.writeShellApplication {
+                    name = "cmd-terminal-cwd";
+                    runtimeInputs = with pkgs; [
+                        coreutils
+                        gawk
+                        gnugrep
+                        hyprland
+                        procps
+                    ];
+
+                    text = ''
+                        terminal_pid=$(hyprctl activewindow | awk '/pid:/ {print $2}')
+                        shell_pid=$(pgrep -P "$terminal_pid" | tail -n1)
+
+                        if [[ -n "$shell_pid" ]]; then
+                            cwd=$(readlink -f "/proc/$shell_pid/cwd" 2>/dev/null)
+                            shell=$(readlink -f "/proc/$shell_pid/exe" 2>/dev/null)
+
+                            if grep -qs "$shell" /etc/shells && [[ -d "$cwd" ]]; then
+                                echo "$cwd"
+                            else
+                                echo "$HOME"
+                            fi
+                        else
+                            echo "$HOME"
+                        fi
+                    '';
+                })
+            ];
+        };
 }
