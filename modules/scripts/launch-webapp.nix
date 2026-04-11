7eab0e17feb9af79e42f5a8d64ce6d82cb148ec4
Simon Mertins <simon@mertins.net>
2026-04-11 12:14:00 +0200

    test

--- modules/scripts/launch-webapp.nix
+++ modules/scripts/launch-webapp.nix
@@ -1,18 +1,22 @@
-{ config, pkgs, ... }:
-let
-    chromiumCmd = "${config.programs.chromium.package}/bin/chromium";
-in
+{ ... }:
 {
-    home.packages = [
-        (pkgs.writeShellApplication {
-            name = "launch-webapp";
-            runtimeInputs = with pkgs; [
-                util-linux
-                uwsm
+    flake.homeModules.scripts =
+        { config, pkgs, ... }:
+        let
+            chromiumCmd = "${config.programs.chromium.package}/bin/chromium";
+        in
+        {
+            home.packages = [
+                (pkgs.writeShellApplication {
+                    name = "launch-webapp";
+                    runtimeInputs = with pkgs; [
+                        util-linux
+                        uwsm
+                    ];
+                    text = ''
+                        exec setsid uwsm app -- ${chromiumCmd} --app="$1" "''${@:2}"
+                    '';
+                })
             ];
-            text = ''
-                exec setsid uwsm app -- ${chromiumCmd} --app="$1" "''${@:2}"
-            '';
-        })
-    ];
+        };
 }
