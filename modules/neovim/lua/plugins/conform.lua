8679a05e53badfe7c8ead7499bcc48abfbf7d0f5
Simon Mertins <simon@mertins.net>
2026-04-12 20:53:35 +0200

    dunno

--- modules/neovim/lua/plugins/conform.lua
+++ modules/neovim/lua/plugins/conform.lua
@@ -41,19 +41,6 @@ return {
 				rust = { "rustfmt" },
 				nix = { "alejandra" },
 			},
-			keys = {
-				{
-					"<leader>f",
-					function()
-						require("conform").format({
-							async = true,
-							lsp_format = "fallback",
-						})
-					end,
-					mode = "",
-					desc = "[F]ormat buffer",
-				},
-			},
 		})
 	end,
 }
