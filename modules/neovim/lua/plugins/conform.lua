aac7708eb9b0efd51b2247b1742323307d0dcb67
Simon Mertins <simon@mertins.net>
2026-04-12 20:47:18 +0200

    dunno

--- modules/neovim/lua/plugins/conform.lua
+++ modules/neovim/lua/plugins/conform.lua
@@ -1,60 +1,59 @@
 return {
-    "conform-nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
+	"conform-nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 
-    after = function()
-        require("conform").setup({
-            notify_on_error = true,
+	after = function()
+		require("conform").setup({
+			notify_on_error = true,
 
-            format_on_save = function(bufnr)
-                local disable_filetypes = {
-                    c = true,
-                    cpp = true,
-                }
+			format_on_save = function(bufnr)
+				local disable_filetypes = {
+					c = true,
+					cpp = true,
+				}
 
-                local lsp_format_opt
-                if disable_filetypes[vim.bo[bufnr].filetype] then
-                    lsp_format_opt = "never"
-                else
-                    lsp_format_opt = "fallback"
-                end
+				local lsp_format_opt
+				if disable_filetypes[vim.bo[bufnr].filetype] then
+					lsp_format_opt = "never"
+				else
+					lsp_format_opt = "fallback"
+				end
 
-                return {
-                    timeout_ms = 500,
-                    lsp_format = lsp_format_opt,
-                }
-            end,
+				return {
+					timeout_ms = 500,
+					lsp_format = lsp_format_opt,
+				}
+			end,
 
-            formatters_by_ft = {
-                python = { "ruff" },
-                lua = { "stylua" },
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                typescriptreact = { "prettier" },
-                go = { "gofmt" },
-                html = { "prettier" },
-                astro = { "prettier" },
-                json = { "jq" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                rust = { "rustfmt" },
-		nix = { "alejandra" },
-            },
-        })
-    end,
-
-    keys = {
-        {
-            "<leader>f",
-            function()
-                require("conform").format({
-                    async = true,
-                    lsp_format = "fallback",
-                })
-            end,
-            mode = "",
-            desc = "[F]ormat buffer",
-        },
-    },
+			formatters_by_ft = {
+				python = { "ruff" },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				go = { "gofmt" },
+				html = { "prettier" },
+				astro = { "prettier" },
+				json = { "jq" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				rust = { "rustfmt" },
+				nix = { "alejandra" },
+			},
+			keys = {
+				{
+					"<leader>f",
+					function()
+						require("conform").format({
+							async = true,
+							lsp_format = "fallback",
+						})
+					end,
+					mode = "",
+					desc = "[F]ormat buffer",
+				},
+			},
+		})
+	end,
 }
--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -1,63 +1,71 @@
-{ self, inputs, ... }: {
-    flake.nixosModules.neovim = { pkgs, ... }: {
-        programs.neovim = {
-            enable = true;
-            package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNeovim;
-        };
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.neovim = {pkgs, ...}: {
+    programs.neovim = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNeovim;
     };
-
-    perSystem = { pkgs, ... }: {
-        packages.myNeovim = inputs.wrapper-modules.wrappers.neovim.wrap {
-            inherit pkgs;
-
-            specs.initLua = {
-                data = null;
-                before = [ "MAIN_INIT" ];
-                config = ''
-                    require('init')
-                '';
-            };
-
-            specs.general = with pkgs.vimPlugins; [
-	    conform-nvim
-	    	rose-pine
-		colorful-menu-nvim
-                nvim-lspconfig
-                nvim-treesitter.withAllGrammars
-                lz-n
-                oil-nvim
-    	        lspkind-nvim
-                blink-cmp
-                nvim-web-devicons
-            ];
-		  specs.lazy = {
-    lazy = true;
-    data = with pkgs.vimPlugins; [
-   lazydev-nvim
-    ];
   };
 
-            info = {
-                values = "for lua";
-                which = "will be placed in the generated info plugin for access";
-            };
+  perSystem = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    packages.myNeovim = inputs.wrapper-modules.wrappers.neovim.wrap {
+      inherit pkgs;
 
-            extraPackages = with pkgs; [
-                lua-language-server
-                astro-language-server
-                typescript-language-server
-                rust-analyzer
-                nixd
-                alejandra
- 
-		# formatters
-		prettier
-		stylua
-		go
-		rustfmt
-            ];
+      specs.initLua = {
+        data = null;
+        before = ["MAIN_INIT"];
+        config = ''
+          require('init')
+        '';
+      };
 
-            settings.config_directory = ./.;
-        };
+      specs.general = with pkgs.vimPlugins; [
+        conform-nvim
+        rose-pine
+        colorful-menu-nvim
+        nvim-lspconfig
+        nvim-treesitter.withAllGrammars
+        lz-n
+        oil-nvim
+        lspkind-nvim
+        blink-cmp
+        nvim-web-devicons
+      ];
+      specs.lazy = {
+        lazy = true;
+        data = with pkgs.vimPlugins; [
+          lazydev-nvim
+        ];
+      };
+
+      info = {
+        values = "for lua";
+        which = "will be placed in the generated info plugin for access";
+      };
+
+      extraPackages = with pkgs; [
+        lua-language-server
+        astro-language-server
+        typescript-language-server
+        rust-analyzer
+        nixd
+        alejandra
+
+        # formatters
+        prettier
+        stylua
+        go
+        rustfmt
+      ];
+
+      settings.config_directory = lib.generators.mkLuaInline "vim.fn.stdpath('config')";
     };
+  };
 }
