b0d10e2e6286ac5678353e096c2aeadd813e6d6f
Simon Mertins <simon@mertins.net>
2026-04-12 20:17:35 +0200

    dunno

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -10,51 +10,46 @@
         packages.myNeovim = inputs.wrapper-modules.wrappers.neovim.wrap {
             inherit pkgs;
 
-            config = { wlib, pkgs, lib, ... }: {
-                imports = [ wlib.wrapperModules.neovim ];
-    	        specs.initLua = {
-			data = null;
-			before = ["MAIN_INIT"];
-			config = ''
-			  require('init')
-			'';
-		      };
-
-
-                specs.general = with pkgs.vimPlugins; [
-                    nvim-lspconfig
-		    nvim-treesitter.withAllGrammars
-		    ln-z
-		    oil
-		    blink-cmp
-		    nvim-web-devicons
-                ];
-
-                specs.lazy = {
-                    lazy = true;
-                    data = with pkgs.vimPlugins; [
-                        # plugins which are not loaded until you vim.cmd.packadd them ...
-                    ];
-                };
-
-                info = {
-                    values = "for lua";
-                    which = "will be placed in the generated info plugin for access";
-                };
-
-                extraPackages = with pkgs; [
-                            lua-language-server
-        astro-language-server
-        typescript-language-server
-        rust-analyzer
-        kdePackages.qtdeclarative
-        nixd
-        alejandra
-                ];
-
-                settings.config_directory = ./.;
-                # or lib.generators.mkLuaInline "vim.fn.stdpath('config')";
+            specs.initLua = {
+                data = null;
+                before = [ "MAIN_INIT" ];
+                config = ''
+                    require('init')
+                '';
             };
+
+            specs.general = with pkgs.vimPlugins; [
+	    	rose-pine
+                nvim-lspconfig
+                nvim-treesitter.withAllGrammars
+                lz-n
+                oil-nvim
+    	        lspkind-nvim
+                blink-cmp
+                nvim-web-devicons
+            ];
+		  specs.lazy = {
+    lazy = true;
+    data = with pkgs.vimPlugins; [
+   lazydev-nvim
+    ];
+  };
+
+            info = {
+                values = "for lua";
+                which = "will be placed in the generated info plugin for access";
+            };
+
+            extraPackages = with pkgs; [
+                lua-language-server
+                astro-language-server
+                typescript-language-server
+                rust-analyzer
+                nixd
+                alejandra
+            ];
+
+            settings.config_directory = ./.;
         };
     };
 }
