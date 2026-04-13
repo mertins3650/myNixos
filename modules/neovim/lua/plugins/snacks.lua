6b35a9b44d3ff906ea36470284d1f834480c7e24
Simon Mertins <simon@mertins.net>
2026-04-13 08:57:22 +0200

    snacks

--- modules/neovim/lua/plugins/snacks.lua
+++ modules/neovim/lua/plugins/snacks.lua
@@ -2,84 +2,50 @@ return {
 	"snacks.nvim",
 	after = function()
 		require("snacks").setup({
-			opts = {
-				picker = {
-					enabled = true,
-					layout = "telescope",
-				},
-			},
-			keys = {
-				{
-					"<leader>sf",
-					function()
-						Snacks.picker.files({ hidden = true })
-					end,
-					desc = "[F]iles",
-				},
-				{
-					"<leader>gf",
-					function()
-						Snacks.picker.git_files()
-					end,
-					desc = "Find Git Files",
-				},
-				{
-					"<leader>gb",
-					function()
-						Snacks.picker.git_branches()
-					end,
-					desc = "Git Branches",
-				},
-				{
-					"<leader>gl",
-					function()
-						Snacks.picker.git_log()
-					end,
-					desc = "Git Log",
-				},
-				{
-					"<leader>gL",
-					function()
-						Snacks.picker.git_log_line()
-					end,
-					desc = "Git Log Line",
-				},
-				{
-					"<leader>gs",
-					function()
-						Snacks.picker.git_status()
-					end,
-					desc = "Git Status",
-				},
-				{
-					"<leader>gS",
-					function()
-						Snacks.picker.git_stash()
-					end,
-					desc = "Git Stash",
-				},
-				{
-					"<leader>gd",
-					function()
-						Snacks.picker.git_diff()
-					end,
-					desc = "Git Diff (Hunks)",
-				},
-				{
-					"<leader>gf",
-					function()
-						Snacks.picker.git_log_file()
-					end,
-					desc = "Git Log File",
-				},
-				{
-					"<leader>sg",
-					function()
-						Snacks.picker.grep()
-					end,
-					desc = "[G]rep",
-				},
+			picker = {
+				enabled = true,
+				layout = "telescope",
 			},
 		})
+
+		vim.keymap.set("n", "<leader>sf", function()
+			Snacks.picker.files({ hidden = true })
+		end, { desc = "[F]iles" })
+
+		vim.keymap.set("n", "<leader>gf", function()
+			Snacks.picker.git_files()
+		end, { desc = "Find Git Files" })
+
+		vim.keymap.set("n", "<leader>gb", function()
+			Snacks.picker.git_branches()
+		end, { desc = "Git Branches" })
+
+		vim.keymap.set("n", "<leader>gl", function()
+			Snacks.picker.git_log()
+		end, { desc = "Git Log" })
+
+		vim.keymap.set("n", "<leader>gL", function()
+			Snacks.picker.git_log_line()
+		end, { desc = "Git Log Line" })
+
+		vim.keymap.set("n", "<leader>gs", function()
+			Snacks.picker.git_status()
+		end, { desc = "Git Status" })
+
+		vim.keymap.set("n", "<leader>gS", function()
+			Snacks.picker.git_stash()
+		end, { desc = "Git Stash" })
+
+		vim.keymap.set("n", "<leader>gd", function()
+			Snacks.picker.git_diff()
+		end, { desc = "Git Diff (Hunks)" })
+
+		vim.keymap.set("n", "<leader>gF", function()
+			Snacks.picker.git_log_file()
+		end, { desc = "Git Log File" })
+
+		vim.keymap.set("n", "<leader>sg", function()
+			Snacks.picker.grep()
+		end, { desc = "[G]rep" })
 	end,
 }
