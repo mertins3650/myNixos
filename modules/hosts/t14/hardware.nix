1b5143a8288329451dd89a702df4b26217fbd511
Simon Mertins <simon@mertins.net>
2026-04-16 12:27:02 +0200

    new

--- modules/hosts/t14/hardware.nix
+++ modules/hosts/t14/hardware.nix
@@ -1,37 +1,37 @@
-
-{ self, inputs, ... }: {
-
-  flake.nixosModules.t14Hardware = { config, lib, pkgs, modulesPath, ... }: {
-
-imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+{...}: {
+  flake.nixosModules.t14Hardware = {
+    config,
+    lib,
+    modulesPath,
+    ...
+  }: {
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci_renesas" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = ["nvme" "xhci_pci_renesas" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-amd"];
+    boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "/dev/mapper/luks-911434da-cb89-4044-9d68-68c772467cd4";
+    fileSystems."/" = {
+      device = "/dev/mapper/luks-d0cce0c7-7905-48f2-bba0-a2a538c97e06";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-911434da-cb89-4044-9d68-68c772467cd4".device = "/dev/disk/by-uuid/911434da-cb89-4044-9d68-68c772467cd4";
+    boot.initrd.luks.devices."luks-d0cce0c7-7905-48f2-bba0-a2a538c97e06".device = "/dev/disk/by-uuid/d0cce0c7-7905-48f2-bba0-a2a538c97e06";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/04D0-6F20";
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/7014-4436";
       fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
+      options = ["fmask=0077" "dmask=0077"];
     };
 
-  swapDevices =
-    [ { device = "/dev/mapper/luks-3645a30c-d4ed-4817-a05e-61e9e4adba0b"; }
+    swapDevices = [
+      {device = "/dev/mapper/luks-d9bb10f0-216f-4784-b8a1-f52c0f323447";}
     ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-};
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
-
