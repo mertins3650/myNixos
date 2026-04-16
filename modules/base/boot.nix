01cb3f220b779cb545578ba143dd53194a2b2d21
Simon Mertins <simon@mertins.net>
2026-04-16 11:40:54 +0200

    upd

--- modules/base/boot.nix
+++ modules/base/boot.nix
@@ -1,94 +1,81 @@
-{ self, input, ... }: {
-    flake.nixosModules.base = { pkgs, lib, ... }:
-    let
-        archPlymouthTheme = pkgs.stdenvNoCC.mkDerivation {
-            pname = "arch-plymouth-theme";
-            version = "1.0";
-            src = ../../defaults/plymouth;
-            dontBuild = true;
+{
+  self,
+  input,
+  ...
+}: {
+  flake.nixosModules.base = {
+    pkgs,
+    lib,
+    ...
+  }: let
+    archPlymouthTheme = pkgs.stdenvNoCC.mkDerivation {
+      pname = "arch-plymouth-theme";
+      version = "1.0";
+      src = ../../defaults/plymouth;
+      dontBuild = true;
 
-            installPhase = ''
-                themeDir=$out/share/plymouth/themes/arch
+      installPhase = ''
+        themeDir=$out/share/plymouth/themes/arch
 
-                mkdir -p "$themeDir"
-                cp -r ./* "$themeDir"/
+        mkdir -p "$themeDir"
+        cp -r ./* "$themeDir"/
 
-                substituteInPlace "$themeDir/arch.plymouth" \
-                    --replace-fail 'ImageDir=.' "ImageDir=$themeDir" \
-                    --replace-fail 'ScriptFile=arch.script' "ScriptFile=$themeDir/arch.script"
-            '';
-        };
-    in {
-        boot.kernelPackages = pkgs.linuxPackages_latest;
-
-        boot = {
-            kernelParams = [
-                "quiet"
-                "splash"
-                "udev.log_level=3"
-                "systemd.show_status=auto"
-            ];
-
-            consoleLogLevel = 3;
-
-            initrd = {
-                verbose = false;
-                systemd.enable = true;
-            };
-
-            plymouth = {
-                enable = true;
-                theme = "arch";
-                themePackages = [ archPlymouthTheme ];
-            };
-
-            loader = {
-                timeout = 3;
-
-                efi.canTouchEfiVariables = true;
-
-                limine = {
-                    enable = true;
-                    maxGenerations = 5;
-                    panicOnChecksumMismatch = false;
-
-                    extraConfig = ''
-                        interface_branding: NixOS
-                        interface_branding_color: 2
-
-                        term_background: 1a1b26
-                        backdrop: 1a1b26
-
-                        term_palette: 15161e;f7768e;9ece6a;e0af68;7aa2f7;bb9af7;7dcfff;a9b1d6
-                        term_palette_bright: 414868;f7768e;9ece6a;e0af68;7aa2f7;bb9af7;7dcfff;c0caf5
-
-                        term_foreground: c0caf5
-                        term_foreground_bright: c0caf5
-                        term_background_bright: 24283b
-                    '';
-                };
-            };
-        };
-
-        services.displayManager = {
-            defaultSession = "hyprland-uwsm";
-            sddm = {
-                enable = true;
-                wayland.enable = true;
-            };
-            autoLogin = {
-                enable = true;
-                user = "simonm";
-            };
-        };
-
-        services.gnome.gnome-keyring.enable = true;
-        security.pam.services.sddm.enableGnomeKeyring = true;
-        systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
-
-        security.pam.services.sddm-autologin.text = pkgs.lib.mkBefore ''
-            auth optional ${pkgs.systemd}/lib/security/pam_systemd_loadkey.so
-            auth include sddm
-        '';
+        substituteInPlace "$themeDir/arch.plymouth" \
+            --replace-fail 'ImageDir=.' "ImageDir=$themeDir" \
+            --replace-fail 'ScriptFile=arch.script' "ScriptFile=$themeDir/arch.script"
+      '';
     };
+  in {
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    boot = {
+      kernelParams = [
+        "quiet"
+        "splash"
+        "udev.log_level=3"
+        "systemd.show_status=auto"
+      ];
+
+      consoleLogLevel = 3;
+
+      initrd = {
+        verbose = false;
+        systemd.enable = true;
+      };
+
+      plymouth = {
+        enable = true;
+        theme = "arch";
+        themePackages = [archPlymouthTheme];
+      };
+
+      loader = {
+        timeout = 3;
+        systemd-boot.enalbe = true;
+
+        efi.canTouchEfiVariables = true;
+      };
+    };
+
+    services.displayManager = {
+      defaultSession = "hyprland-uwsm";
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      autoLogin = {
+        enable = true;
+        user = "simonm";
+      };
+    };
+
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.sddm.enableGnomeKeyring = true;
+    systemd.services.display-manager.serviceConfig.KeyringMode = "inherit";
+
+    security.pam.services.sddm-autologin.text = pkgs.lib.mkBefore ''
+      auth optional ${pkgs.systemd}/lib/security/pam_systemd_loadkey.so
+      auth include sddm
+    '';
+  };
 }
