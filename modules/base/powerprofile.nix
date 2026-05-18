e5666724f2839cec1d123f844463d8265bb9deb9
Simon Mertins <simon@mertins.net>
2026-05-18 22:08:02 +0200

    update

--- modules/base/powerprofile.nix
+++ modules/base/powerprofile.nix
@@ -1,10 +1,7 @@
-{ lib, config, pkgs, ... }:
-{
-    flake.nixosModules.base = {
-        services.power-profiles-daemon.enable = true;
-        powerManagement.powertop.enable = true;
-
-        # Easy to override per-host if a machine does not benefit from it.
-        services.thermald.enable = lib.mkDefault false;
-    };
+{lib, ...}: {
+  flake.nixosModules.base = {
+    services.power-profiles-daemon.enable = true;
+    powerManagement.powertop.enable = false;
+    services.thermald.enable = lib.mkDefault false;
+  };
 }
--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -1,47 +1,47 @@
-{ self, inputs, ... }: {
-    flake.nixosModules.desktopenv = { pkgs, lib, ... }: {
-        programs.hyprland = {
-            enable = true;
-            withUWSM = true;
-        };
-
-        services.udisks2.enable = true;
-        services.devmon.enable = true;
-        services.gvfs.enable = true;
-        services.xserver.updateDbusEnvironment = true;
-
-        xdg.portal = {
-            enable = true;
-            xdgOpenUsePortal = true;
-
-            extraPortals = [
-                pkgs.xdg-desktop-portal-hyprland
-                pkgs.xdg-desktop-portal-gtk
-            ];
-
-            config = {
-                common = {
-                    default = [ "hyprland" "gtk" ];
-                    "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-                };
-
-                hyprland = {
-                    default = [ "hyprland" "gtk" ];
-                    "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-                };
-            };
-        };
-
-        environment.systemPackages = [
-            pkgs.wl-clipboard
-            pkgs.nautilus
-            pkgs.glib
-            pkgs.swaybg
-            pkgs.brightnessctl
-            pkgs.yaru-theme
-            pkgs.bluetui
-            pkgs.wiremix
-            pkgs.impala
-        ];
+{...}: {
+  flake.nixosModules.desktopenv = {pkgs, ...}: {
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
     };
+
+    services.udisks2.enable = true;
+    services.devmon.enable = true;
+    services.gvfs.enable = true;
+    services.xserver.updateDbusEnvironment = true;
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+
+      extraPortals = [
+        pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
+      ];
+
+      config = {
+        common = {
+          default = ["hyprland" "gtk"];
+          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+        };
+
+        hyprland = {
+          default = ["hyprland" "gtk"];
+          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+        };
+      };
+    };
+
+    environment.systemPackages = [
+      pkgs.wl-clipboard
+      pkgs.nautilus
+      pkgs.glib
+      pkgs.swaybg
+      pkgs.brightnessctl
+      pkgs.yaru-theme
+      pkgs.bluetui
+      pkgs.wiremix
+      pkgs.impala
+    ];
+  };
 }
