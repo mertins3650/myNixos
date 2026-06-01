090e0be92770f2118526d7108ab69ab9e1a98b22
Simon Mertins <simon@mertins.net>
2026-06-01 13:49:22 +0200

    docker+update

--- modules/hosts/desktop/configuration.nix
+++ modules/hosts/desktop/configuration.nix
@@ -6,45 +6,45 @@
   flake.nixosModules.desktopConfiguration = {...}: {
     imports = [
       inputs.home-manager.nixosModules.home-manager
+      self.nixosModules.base
       self.nixosModules.desktopHardware
-      self.nixosModules.homemanager
-      self.nixosModules.gaming
       self.nixosModules.desktopenv
       self.nixosModules.development
-      self.nixosModules.base
+      self.nixosModules.docker
+      self.nixosModules.gaming
+      self.nixosModules.homemanager
       self.nixosModules.neovim
     ];
 
     home-manager.users.simonm.imports = [
-      self.homeModules.hyprland
-      self.homeModules.hypridle
-      self.homeModules.fcitx5
-      self.homeModules.nh
-      self.homeModules.hyprlock
-      self.homeModules.waybar
       self.homeModules.chromium
-      self.homeModules.keyring
-      self.homeModules.scripts
-      self.homeModules.terminal
-      self.homeModules.theme
-      self.homeModules.tmux
-      self.homeModules.rofi
-      self.homeModules.shell
-      self.homeModules.swayosd
-      self.homeModules.mako
+      self.homeModules.fcitx5
       self.homeModules.fontconfig
       self.homeModules.git
       self.homeModules.homepackages
+      self.homeModules.hypridle
+      self.homeModules.hyprland
+      self.homeModules.hyprlock
+      self.homeModules.keyring
+      self.homeModules.mako
+      self.homeModules.nh
+      self.homeModules.rofi
+      self.homeModules.scripts
+      self.homeModules.shell
+      self.homeModules.swayosd
+      self.homeModules.terminal
+      self.homeModules.theme
+      self.homeModules.tmux
+      self.homeModules.waybar
     ];
-  boot.initrd.luks.devices."luks-aef71664-5c85-4be2-a5f0-aab09ab77baf".device = "/dev/disk/by-uuid/aef71664-5c85-4be2-a5f0-aab09ab77baf";
-
+    boot.initrd.luks.devices."luks-aef71664-5c85-4be2-a5f0-aab09ab77baf".device = "/dev/disk/by-uuid/aef71664-5c85-4be2-a5f0-aab09ab77baf";
 
     services.thermald.enable = true;
-services.xserver.videoDrivers = ["amdgpu"];
+    services.xserver.videoDrivers = ["amdgpu"];
     boot.initrd.kernelModules = ["amdgpu"];
     networking.hostName = "desktop"; # Define your hostname.
- hardware.cpu.amd.updateMicrocode = true;
-hardware.graphics.enable = true;
+    hardware.cpu.amd.updateMicrocode = true;
+    hardware.graphics.enable = true;
 
     system.stateVersion = "25.11"; # Did you read the comment?
   };
