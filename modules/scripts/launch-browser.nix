9172ac75412276f7e511a0e163550128a1362903
Simon Mertins <simon@mertins.net>
2026-06-12 19:58:20 +0200

    zen

--- modules/scripts/launch-browser.nix
+++ modules/scripts/launch-browser.nix
@@ -1,3 +1,43 @@
+# {...}: {
+#   flake.homeModules.scripts = {
+#     config,
+#     pkgs,
+#     ...
+#   }: {
+#     home.packages = [
+#       (pkgs.writeShellApplication {
+#         name = "launch-browser";
+#         runtimeInputs = with pkgs; [
+#           util-linux
+#           uwsm
+#         ];
+#
+#         text = ''
+#           browser="${config.programs.chromium.package}/bin/chromium"
+#
+#           if [[ "$browser" =~ (firefox|zen|librewolf|mullvad) ]]; then
+#               private_flag="--private-window"
+#           elif [[ "$browser" =~ edge ]]; then
+#               private_flag="--inprivate"
+#           else
+#               private_flag="--incognito"
+#           fi
+#
+#           args=()
+#           for arg in "$@"; do
+#               if [[ "$arg" == "--private" ]]; then
+#                   args+=("$private_flag")
+#               else
+#                   args+=("$arg")
+#               fi
+#           done
+#
+#           exec setsid uwsm app -- "$browser" "''${args[@]}"
+#         '';
+#       })
+#     ];
+#   };
+# }
 {...}: {
   flake.homeModules.scripts = {
     config,
@@ -13,7 +53,7 @@
         ];
 
         text = ''
-          browser="${config.programs.chromium.package}/bin/chromium"
+          browser="${pkgs.zen-browser}/bin/zen"
 
           if [[ "$browser" =~ (firefox|zen|librewolf|mullvad) ]]; then
               private_flag="--private-window"
--- modules/scripts/launch-webapp.nix
+++ modules/scripts/launch-webapp.nix
@@ -1,10 +1,29 @@
+# {...}: {
+#   flake.homeModules.scripts = {
+#     config,
+#     pkgs,
+#     ...
+#   }: let
+#     chromiumCmd = "${config.programs.chromium.package}/bin/chromium";
+#   in {
+#     home.packages = [
+#       (pkgs.writeShellApplication {
+#         name = "launch-webapp";
+#         runtimeInputs = with pkgs; [
+#           util-linux
+#           uwsm
+#         ];
+#         text = ''
+#           exec setsid uwsm app -- ${chromiumCmd} --app="$1" "''${@:2}"
+#         '';
+#       })
+#     ];
+#   };
+# }
 {...}: {
-  flake.homeModules.scripts = {
-    config,
-    pkgs,
-    ...
-  }: let
-    chromiumCmd = "${config.programs.chromium.package}/bin/chromium";
+  flake.homeModules.scripts = {pkgs, ...}: let
+    # Assuming 'zen-browser' is available in your pkgs via a flake input or overlay
+    zenCmd = "${pkgs.zen-browser}/bin/zen";
   in {
     home.packages = [
       (pkgs.writeShellApplication {
@@ -14,7 +33,8 @@
           uwsm
         ];
         text = ''
-          exec setsid uwsm app -- ${chromiumCmd} --app="$1" "''${@:2}"
+          # Zen does not support Chromium's --app flag, so we pass the URL directly.
+          exec setsid uwsm app -- ${zenCmd} "$1" "''${@:2}"
         '';
       })
     ];
