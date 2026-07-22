c6ec29a21b40222a3fc6dffce66113fa2b29f3e0
Simon Mertins <simon@mertins.net>
2026-07-22 21:31:11 +0200

    new laptop

--- modules/hosts/t14/configuration.nix
+++ modules/hosts/t14/configuration.nix
@@ -3,47 +3,52 @@
   inputs,
   ...
 }: {
-  flake.nixosModules.t14Configuration = {...}: {
+  flake.nixosModules.t14Configuration = {pkgs, ...}: {
     imports = [
       inputs.home-manager.nixosModules.home-manager
+      self.nixosModules.base
       self.nixosModules.t14Hardware
-      self.nixosModules.homemanager
-      self.nixosModules.gaming
       self.nixosModules.desktopenv
       self.nixosModules.development
-      self.nixosModules.base
+      self.nixosModules.docker
+      self.nixosModules.gaming
+      self.nixosModules.homemanager
       self.nixosModules.neovim
     ];
 
     home-manager.users.simonm.imports = [
       self.homeModules.zen
       self.homeModules.defaultapps
-      self.homeModules.hyprland
-      self.homeModules.hypridle
-      self.homeModules.fcitx5
-      self.homeModules.nh
-      self.homeModules.hyprlock
-      self.homeModules.waybar
       self.homeModules.chromium
-      self.homeModules.keyring
-      self.homeModules.scripts
-      self.homeModules.terminal
-      self.homeModules.theme
-      self.homeModules.tmux
-      self.homeModules.rofi
-      self.homeModules.shell
-      self.homeModules.swayosd
-      self.homeModules.mako
+      self.homeModules.fcitx5
       self.homeModules.fontconfig
       self.homeModules.git
       self.homeModules.homepackages
+      self.homeModules.hypridle
+      self.homeModules.hyprland
+      self.homeModules.hyprlock
+      self.homeModules.keyring
+      self.homeModules.mako
+      self.homeModules.nh
+      self.homeModules.rofi
+      self.homeModules.scripts
+      self.homeModules.shell
+      self.homeModules.swayosd
+      self.homeModules.terminal
+      self.homeModules.theme
+      self.homeModules.tmux
+      self.homeModules.waybar
     ];
-    boot.initrd.luks.devices."luks-36de1782-d627-41c9-8c32-902f06423faa".device = "/dev/disk/by-uuid/36de1782-d627-41c9-8c32-902f06423faa";
 
     services.thermald.enable = true;
-    boot.initrd.kernelModules = ["i915"];
+    services.xserver.videoDrivers = ["amdgpu"];
+    boot.initrd.kernelModules = ["amdgpu"];
     networking.hostName = "t14"; # Define your hostname.
-    hardware.cpu.intel.updateMicrocode = true;
+    hardware.cpu.amd.updateMicrocode = true;
+    hardware.graphics.enable = true;
+    environment.systemPackages = with pkgs; [
+      orca-slicer
+    ];
 
     system.stateVersion = "25.11"; # Did you read the comment?
   };
--- modules/hosts/t14/hardware.nix
+++ modules/hosts/t14/hardware.nix
@@ -9,30 +9,32 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
+
 
   fileSystems."/" =
-    { device = "/dev/mapper/luks-e307504c-9a5f-44bd-ac4a-a0b97f5bc707";
+    { device = "/dev/mapper/luks-289d7bb9-103f-4ff8-9db2-ecefaa807f09";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-e307504c-9a5f-44bd-ac4a-a0b97f5bc707".device = "/dev/disk/by-uuid/e307504c-9a5f-44bd-ac4a-a0b97f5bc707";
+  boot.initrd.luks.devices."luks-289d7bb9-103f-4ff8-9db2-ecefaa807f09".device = "/dev/disk/by-uuid/289d7bb9-103f-4ff8-9db2-ecefaa807f09";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C0FE-5003";
+    { device = "/dev/disk/by-uuid/E5EB-4D25";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/mapper/luks-36de1782-d627-41c9-8c32-902f06423faa"; }
+    [ { device = "/dev/mapper/luks-a3c7b431-2a4d-435a-a79c-d52211cb9d8b"; }
     ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   };
 }
